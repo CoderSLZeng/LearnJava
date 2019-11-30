ab8fd5feefc49f1bc30fcf419a18bc015ec0819c
SLCoderZeng <359297567@qq.com>
2019-11-30 10:12:25 +0800

    v0.2-添加文本输入框’

--- Projects/01-Chat/src/ChatClient.java
+++ Projects/01-Chat/src/ChatClient.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 
 public class ChatClient extends Frame {
+	
+	TextArea taContent = new TextArea(); // 多行文本输入框
+	TextField tfTxt = new TextField(); // 单行文本输入框
 
 	public static void main(String[] args) {
 		new ChatClient().launchFrame();
@@ -12,6 +15,9 @@ public class ChatClient extends Frame {
 	private void launchFrame() {
 		setLocation(400, 300); // 窗口坐标位置
 		this.setSize(300, 300); // 窗口大小
+		add(tfTxt, BorderLayout.SOUTH);  // 上北
+		add(taContent, BorderLayout.NORTH); // 下南
+		pack();
 		setVisible(true); // 显示窗口		
 	}
 
